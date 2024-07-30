64f92b81e7c4f424bd66612f2125f0e0f1827a33
Tomm Huth <tomm.huth@hotmail.com>
2024-07-30 15:45:05 +0200

    ignore vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { 
-    images: {
-        imageSizes: [64, 96, 128, 256],
-        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2200],
-    }
+const nextConfig = {
+  images: {
+    imageSizes: [64, 96, 128, 256],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2200],
+  },
+  headers: [
+    {
+      source: "/",
+      has: [
+        {
+          typ: "host",
+          value: "vercel.app",
+        },
+      ],
+      headers: [
+        {
+          key: "X-Robots-Tag",
+          value: "noindex",
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
