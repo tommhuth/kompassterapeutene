67faee533e66c70e927bc4a3f3d039dff8635a02
Tomm Huth <tomm.huth@hotmail.com>
2024-07-30 16:09:34 +0200

    sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="manifest" href="/site.webmanifest" />
+                <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml" />
             </head>
             <body>
                 <Header />
